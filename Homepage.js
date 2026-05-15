4820f354cfcefdba7710c85e3f440d49fd4c73a9
HSIAO YU SHAN <amy90149810@gmail.com>
2026-05-15 16:22:57 +0800

    新增swiper

--- Homepage.js
+++ Homepage.js
@@ -1,7 +1,29 @@
-$(function(){
-    $(".btn").click(function(){
+$(function () {
+    $(".btn").click(function () {
         alert("收到");
-
     })
+})
 
+$(function () {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
 })
